91a66c83f4cba0ea5be9432a44b3658574d5cce3
Caio <caiocostaee@gmail.com>
2017-12-19 13:46:26 -0300

    make it better

--- php/edit-dados.php
+++ php/edit-dados.php
@@ -26,7 +26,7 @@ $id = $_GET['id'];
   $escreve = fwrite($fp, json_encode($in_json));
 
   if($escreve){
-    echo "<meta HTTP-EQUIV='refresh' CONTENT='0;URL=/chef/aula.php?id=". $id .">";
+    echo "<meta HTTP-EQUIV='refresh' CONTENT='0;URL=/aula.php?id=". $id .">";
   }
   else echo 'falha';
 
