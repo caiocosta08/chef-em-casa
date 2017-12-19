ac7325b1b250c16428ea5a3e8d3a74126be39d4e
Caio <caiocostaee@gmail.com>
2017-12-18 23:21:43 -0300

    make it better

--- php/sessao.php
+++ php/sessao.php
@@ -8,18 +8,12 @@ $dir = $_SERVER['PHP_SELF'];
 $dir = explode('/', $dir);
 $dir = $dir[0];
 if((!isset ($_SESSION['login']) == true) and (!isset ($_SESSION['senha']) == true))
-{   echo "entrou<br>";
-    echo 'variavel dir: ' . $dir . '<br>';
-    echo $_SERVER['PHP_SELF'];
-    echo '<br>';
+{
   if(($dir != '/index.php') && ($dir != 'index.php') && ($_SERVER['PHP_SELF'] != '/index.php') && ($_SERVER['PHP_SELF'] != 'index.php')){
     unset($_SESSION['login']);
     unset($_SESSION['senha']);
-    //header('location: index.php');
+    header('location: index.php');
   }else{
-    echo $_SERVER['PHP_SELF'];
-    echo '<br>';
-    echo $dir;
     unset($_SESSION['login']);
     unset($_SESSION['senha']);
   }
