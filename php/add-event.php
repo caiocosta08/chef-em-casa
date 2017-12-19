1270bd8d79aafc33429f88392eb10656182c200d
Caio <caiocostaee@gmail.com>
2017-12-19 13:55:03 -0300

    make it better

--- php/add-event.php
+++ php/add-event.php
@@ -7,7 +7,8 @@
   $result = pg_query($pg_conn, $sql);
   $in_json = [];
   if (pg_num_rows($result)) {
-    $in_json = json_encode($result);
+      $in_json = pg_fetch_all($result);
+      $in_json = json_encode($in_json);
    // $i = 0;
    // while($row = pg_fetch_row($result)) {
    //   $in_json[$i] = $row;
