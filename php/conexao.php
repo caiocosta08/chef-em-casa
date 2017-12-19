87feedac275f1ba9d0470087c9a67ee1b268a937
Caio <caiocostaee@gmail.com>
2017-12-18 22:35:11 -0300

    make it better

--- php/conexao.php
+++ php/conexao.php
@@ -1,31 +1,31 @@
 <?php
 
 
-# This function reads your DATABASE_URL config var and returns a connection
-# string suitable for pg_connect. Put this in your app.
-function pg_connection_string_from_database_url() {
-  extract(parse_url($_ENV["DATABASE_URL"]));
-  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
-}
-# Here we establish the connection. Yes, that's all.
-$pg_conn = pg_connect(pg_connection_string_from_database_url());
-# Now let's use the connection for something silly just to prove it works:
-$result = pg_query($pg_conn, "SELECT * FROM usuarios");
-$in_json = [];
-$i = 0;
-print "<pre>\n";
-if (!pg_num_rows($result)) {
-  print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
-} else {
-  print "Tables in your database:\n";
-  while ($row = pg_fetch_row($result)) {
-      print("- $row[0]\n");
-      $in_json[$i] = $row;
-      $i++;
-      echo json_encode($in_json);
-  }
-}
-print "\n";
+    # This function reads your DATABASE_URL config var and returns a connection
+    # string suitable for pg_connect. Put this in your app.
+    function pg_connection_string_from_database_url() {
+      extract(parse_url($_ENV["DATABASE_URL"]));
+      return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+    }
+    # Here we establish the connection. Yes, that's all.
+    $pg_conn = pg_connect(pg_connection_string_from_database_url());
+    # Now let's use the connection for something silly just to prove it works:
+    $result = pg_query($pg_conn, "SELECT * FROM usuarios");
+    $in_json = [];
+    $i = 0;
+    print "<pre>\n";
+    if (!pg_num_rows($result)) {
+      print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
+    } else {
+      print "Tables in your database:\n";
+      while ($row = pg_fetch_row($result)) {
+          print("- $row[0]\n");
+          $in_json[$i] = $row;
+          $i++;
+          echo json_encode($in_json);
+      }
+    }
+    print "\n";
 
 
 /*# This function reads your DATABASE_URL config var and returns a connection
@@ -55,25 +55,6 @@ $servername = "ec2-23-21-236-249.compute-1.amazonaws.com";
  $dbname = "d7reg9fb1vjb28";
  $porta = 5432;
  $dburl = "postgres://uqahcioaoktplr:321ad2c23bf722fbeffc77c919e7a5d31dc37bef735bd4486f6f653b6ebc1f31@ec2-23-21-236-249.compute-1.amazonaws.com:5432/d7reg9fb1vjb28";
-
- $dbopts = parse_url(getenv($dburl));
- $app->register(new Herrera\Pdo\PdoServiceProvider(),
-                array(
-                    'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts[$servername] . ';port=' . $dbopts["5432"],
-                    'pdo.username' => $dbopts[$username],
-                    'pdo.password' => $dbopts[$password]
-                )
- );
-
-/*
- // Create connection
- $conn = pg_connect("host=$servername port=$porta dbname=$dbname " + "user=$username password=$password");
- // Check connection
- if ($conn->connect_error) {
-   die("Connection failed: " . $conn->connect_error);
- }else{
-   //echo 'CONNECTION OK! ';
-   //echo $conn;
-}*/
+*/
 
 ?>
