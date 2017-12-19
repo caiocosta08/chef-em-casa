eae3c95ac8a1621756f291e5760b469a8bdf7359
Caio <caiocostaee@gmail.com>
2017-12-18 22:32:08 -0300

    make it better

--- php/salvar-dados.php
+++ php/salvar-dados.php
@@ -1,21 +1,37 @@
 <?php
 
-include_once('conexao.php');
+//include_once('conexao.php');
 
  $sql = "SELECT * FROM news ORDER BY id DESC";
 
- $result = pg_query($conn, $sql);
- $in_json = [];
- if ($result) {
-   $i = 0;
-   while($row = pg_fetch_row($result)) {
-     $in_json[$i] = $row;
-     $i++;
-     }
- } else {
-     echo " - 0 resultados de aulas no BD - ";
+ # This function reads your DATABASE_URL config var and returns a connection
+ # string suitable for pg_connect. Put this in your app.
+ function pg_connection_string_from_database_url() {
+   extract(parse_url($_ENV["DATABASE_URL"]));
+   return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
  }
- pg_close($conn);
+ # Here we establish the connection. Yes, that's all.
+ $pg_conn = pg_connect(pg_connection_string_from_database_url());
+ # Now let's use the connection for something silly just to prove it works:
+ $result = pg_query($pg_conn, $sql);
+ $in_json = [];
+ $i = 0;
+ print "<pre>\n";
+ if (!pg_num_rows($result)) {
+   print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
+ } else {
+   print "Tables in your database:\n";
+   while ($row = pg_fetch_row($result)) {
+       print("- $row[0]\n");
+       $in_json[$i] = $row;
+       $i++;
+       echo json_encode($in_json);
+   }
+ }
+ print "\n";
+
+
+
 
   $fp = fopen("../json/dados.json", "w");
   if($fp){
