e4ebdacc33e32b70636ea42975fc69fce395eb70
Caio <caiocostaee@gmail.com>
2017-12-18 22:28:12 -0300

    make it better

--- php/conexao.php
+++ php/conexao.php
@@ -11,12 +11,19 @@ function pg_connection_string_from_database_url() {
 $pg_conn = pg_connect(pg_connection_string_from_database_url());
 # Now let's use the connection for something silly just to prove it works:
 $result = pg_query($pg_conn, "SELECT * FROM usuarios");
+$in_json = [];
+$i = 0;
 print "<pre>\n";
 if (!pg_num_rows($result)) {
   print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
 } else {
   print "Tables in your database:\n";
-  while ($row = pg_fetch_row($result)) { print("- $row\n"); }
+  while ($row = pg_fetch_row($result)) {
+      print("- $row[0]\n");
+      $in_json[$i] = $row;
+      $i++;
+      echo json_encode($in_json);
+  }
 }
 print "\n";
 
