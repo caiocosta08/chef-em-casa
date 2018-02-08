4ece9b5b3b279af74786cc50ae122bf1467671ed
Caio <caiocostaee@gmail.com>
2018-02-08 17:40:17 -0300

    make it better

--- aula.php
+++ aula.php
@@ -114,7 +114,27 @@
 			      <div class="modal-body">
 							<button type="button" class="close" data-dismiss="modal">&times;</button>
 							<h2>Contrate o Chef para o seu evento!</h2>
-							<?php include('forms/edit-aula-form.php'); ?>
+							<!--<?php/* include('forms/contact-chef.php');*/ ?>-->
+							<div id="contact-chef">
+							    <form class="contact-chef" action="" method="post">
+							        <div class="form-group">
+							            <label>Nome do Cliente</label>
+							            <input class="form-control" type="text" name="nome">
+							        </div>
+							        <div class="form-group">
+							            <label>Data do Evento</label>
+							            <input class="form-control" type="date" name="data">
+							        </div>
+							        <div class="form-group">
+							            <label>Tipo do Evento</label>
+							            <input class="form-control" type="text" name="evento">
+							        </div>
+							        <div class="form-group">
+							            <label>Descrição</label>
+							            <input class="form-control" type="text" name="desc">
+							        </div>
+							    </form>
+							</div>
 				  </div>
 			      <div class="modal-footer">
 			        <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
