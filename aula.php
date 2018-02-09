b47fe42527c4cf737d6e904d6c625b3942576abb
Caio <caiocostaee@gmail.com>
2018-02-09 15:15:07 -0300

    atualizando...

--- aula.php
+++ aula.php
@@ -116,27 +116,7 @@
 							<h2>Contrate o Chef para o seu evento!</h2>
 							<!--<?php/* include('forms/contact-chef.php');*/ ?>-->
 							<div id="contact-chef">
-							    <form class="contact-chef" action="" method="post">
-							        <div class="form-group">
-							            <label>Nome do Cliente</label>
-							            <input class="form-control" type="text" name="nome">
-							        </div>
-							        <div class="form-group">
-							            <label>Data do Evento</label>
-							            <input class="form-control" type="date" name="data">
-							        </div>
-							        <div class="form-group">
-							            <label>Tipo do Evento</label>
-							            <input class="form-control" type="text" name="evento">
-							        </div>
-							        <div class="form-group">
-							            <label>Descrição</label>
-							            <input class="form-control" type="text" name="desc">
-							        </div>
-									<div class="input-group-btn">
-										<button type="submit" class="btn btn-success">Contratar <span class="glyphicon glyphicon-success"></span></button>
-									</div>
-							    </form>
+							    
 							</div>
 				  </div>
 			      <div class="modal-footer">
--- forms/contact-chef.php
+++ forms/contact-chef.php
@@ -1,20 +1,21 @@
-<div id="contact-chef">
-    <form class="contact-chef" action="" method="post">
-        <div class="form-group">
-            <label>Nome do Cliente</label>
-            <input class="form-control" type="text" name="nome">
-        </div>
-        <div class="form-group">
-            <label>Data do Evento</label>
-            <input class="form-control" type="date" name="data">
-        </div>
-        <div class="form-group">
-            <label>Tipo do Evento</label>
-            <input class="form-control" type="text" name="evento">
-        </div>
-        <div class="form-group">
-            <label>Descrição</label>
-            <input class="form-control" type="text" name="desc">
-        </div>
-    </form>
-</div>
+<form class="contact-chef" action="" method="post">
+    <div class="form-group">
+        <label>Nome do Cliente</label>
+        <input class="form-control" type="text" name="nome">
+    </div>
+    <div class="form-group">
+        <label>Data do Evento</label>
+        <input class="form-control" type="date" name="data">
+    </div>
+    <div class="form-group">
+        <label>Tipo do Evento</label>
+        <input class="form-control" type="text" name="evento">
+    </div>
+    <div class="form-group">
+        <label>Descrição</label>
+        <input class="form-control" type="text" name="desc">
+    </div>
+    <div class="input-group-btn">
+        <button type="submit" class="btn btn-success">Contratar <span class="glyphicon glyphicon-success"></span></button>
+    </div>
+</form>
