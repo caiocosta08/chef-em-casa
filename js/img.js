67a7591b3b5191e3e96a35cec0d78de08fba98c8
Caio <caiocostaee@gmail.com>
2018-02-10 15:22:48 -0300

    interface de chat com horario

--- js/img.js
+++ js/img.js
@@ -54,9 +54,9 @@ function lerJSON(){
         let msgs = '<ul class="well" style="color:#555555; background-color:#ffffff;  list-style:none;">'
     for (var x of Object.keys(dados)) {
         let id = dados[x].id
-        let idAnterior = dados[x].id
         let mensagens = dados[x].message
         let autor = dados[x].name
+        if(x!=0) let autorAnterior = dados[x-1].name
         
         if(id != idAnterior){
             if(autor == userlogado)
