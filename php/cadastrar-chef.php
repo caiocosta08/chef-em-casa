40e412919f84c4696cb9f31e4652317871a60617
Caio <caiocostaee@gmail.com>
2018-02-09 23:21:16 -0300

    corrigindo send.php - envio de mensagens

--- php/cadastrar-chef.php
+++ php/cadastrar-chef.php
@@ -8,14 +8,7 @@ $resumo = $_POST['resumo'];
 $referencias = $_POST['referencias'];
 
 include('conexao.php');
-$consulta = "SELECT id FROM news ORDER BY id";
-$resultado = pg_query($pg_conn, $consulta);
-echo 'resultados: ' . pg_get_result($resultado);
-echo 'erros: ' . pg_last_error($resultado);
-echo 'numero de linhas: ' . pg_num_rows($resultado);
-$id = pg_num_rows($resultado);
-$id++;
-$link = "chef.php?id=".$id;
+
 //---------------------------------
 $con = pg_connect(pg_connection_string_from_database_url());
 if ($con->connect_error) {
@@ -39,25 +32,5 @@ if($result){
   echo pg_result_error($result);
 }
 
-$query = "INSERT INTO arquivos (nome, tipo, tamanho, conteudo ) "."VALUES ('$fileName', '$fileType', '$fileSize', '$content')";
-$res = pg_query($con, $query);
-if(isset($_POST['upload']) && $_FILES['userfile']['size'] > 0)
-{
-$fileName = $_FILES['userfile']['name'];
-$tmpName  = $_FILES['userfile']['tmp_name'];
-$fileSize = $_FILES['userfile']['size'];
-$fileType = $_FILES['userfile']['type'];
-
-$fp      = fopen($tmpName, 'r');
-$content = fread($fp, filesize($tmpName));
-$content = addslashes($content);
-fclose($fp);
-
-if(!get_magic_quotes_gpc())
-{
-    $fileName = addslashes($fileName);
-}
-
-}
 pg_close($pg_conn);
 ?>
