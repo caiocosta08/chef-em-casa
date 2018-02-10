7b510edb2547fdf5fe77f70da12987cbf912740f
Caio <caiocostaee@gmail.com>
2018-02-10 17:38:03 -0300

    interface de chat com horario

--- chef.php
+++ chef.php
@@ -14,6 +14,7 @@
 <script src="js/functions.js" type="text/javascript"></script>
 <script src="js/validator.js" type="text/javascript"></script>
 <script src="js/img.js" type="text/javascript"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
 <style rel="stylesheet" type="text/css">
 	.navbar {
 		background-color: #fff353;
