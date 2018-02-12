96e51117140c7a3d96d550ff18959ca08cd02f48
Caio <caiocostaee@gmail.com>
2018-02-12 20:07:35 -0300

    editando chatbox

--- chef.php
+++ chef.php
@@ -11,10 +11,7 @@
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-		<script src="js/load.js" type="text/javascript"></script>
-		<script src="js/functions.js" type="text/javascript"></script>
-		<script src="js/validator.js" type="text/javascript"></script>
-		<script src="js/img.js" type="text/javascript"></script>
+		
 	</head>
 	
 	<body>	
@@ -123,6 +120,10 @@
 			<p>Copyright &copy; 2017 Chef em Casa. All Rights Reserved.</p>
 			<p><a href="#">Privacy Policy</a> | <a href="#">Terms of Use</a></p>
 		</footer>
-		<script type="text/javascript" src="js/img.js"></script>
-	</body>
+		<script src="js/load.js" type="text/javascript"></script>
+		<script src="js/functions.js" type="text/javascript"></script>
+		<script src="js/validator.js" type="text/javascript"></script>
+		<script src="js/img.js" type="text/javascript"></script>
+	
+</body>
 </html>
