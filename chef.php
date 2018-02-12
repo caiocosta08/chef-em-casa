369033c60b2e35e0f24f8298a6c2dc815cb642d8
Caio <caiocostaee@gmail.com>
2018-02-12 20:42:01 -0300

    editando chatbox

--- chef.php
+++ chef.php
@@ -11,6 +11,9 @@
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+        <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+
+
 		
 	</head>
 	
