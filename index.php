c53d4a5ab50126991791f650d4c7f847dd10dc04
Caio <caiocostaee@gmail.com>
2018-02-13 16:10:55 -0300

    adicionando analythics

--- index.php
+++ index.php
@@ -10,6 +10,13 @@
 
           gtag('config', 'UA-114078381-1');
         </script>
+        <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PMZNNMW');</script>
+<!-- End Google Tag Manager -->
 
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -29,6 +36,10 @@
 	</head>
 	
 	<body>
+        <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PMZNNMW"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 	
 		<nav class="navbar navbar-expand-sm navbar-dark" style="background: coral; margin-bottom: 20px;">
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-content" aria-controls="nav-content" aria-expanded="false" aria-label="Toggle navigation">
@@ -78,6 +89,7 @@
 							echo '<button type="button" name="button"><a href="php/logout.php">LOGOUT</a> </button>';
 						}else include('forms/login.php');
 					?>
+                    <a href="#" id="ga">CLIQUE</a>
 				</div>
 				<div class="col-md-2 sidenav">
 					<ul id="contentNews" style="list-style: none; padding: 0;"></ul>
