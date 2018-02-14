8821b203cd46e8d25c285355a291914b9d0e8ce9
Caio <caiocostaee@gmail.com>
2018-02-13 21:02:32 -0300

    adicionando analythics - click counter

--- index.php
+++ index.php
@@ -37,23 +37,6 @@
 		<script src="js/ana.js" type="text/javascript"></script>
         <script src='https://www.google.com/recaptcha/api.js'></script>
         
-	
-        <script>
-            // Feature detects Navigation Timing API support.
-            if (window.performance) {
-                // Gets the number of milliseconds since page load
-                // (and rounds the result since the value must be an integer).
-                var timeSincePageLoad = Math.round(performance.now());
-
-                // Sends the timing event to Google Analytics.
-                gtag('event', 'timing_complete', {
-                    'name': 'load',
-                    'value': timeSincePageLoad,
-                    'event_category': 'JS Dependencies'
-                });
-            }
-        </script>
-
     </head>
 	
 	<body>
--- js/ana.js
+++ js/ana.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
     $("#btnLogin").click(function(e){
-        gtag('event', 'login')
-    })
+        gtag('event', 'login', {
+            'event_category': 'login-no-site',
+            'usuario': user
+        })
+    });
+    
+    // Feature detects Navigation Timing API support.
+    if (window.performance) {
+        // Gets the number of milliseconds since page load
+        // (and rounds the result since the value must be an integer).
+        var timeSincePageLoad = Math.round(performance.now());
+
+        // Sends the timing event to Google Analytics.
+        gtag('event', 'timing_complete', {
+            'name': 'load',
+            'value': timeSincePageLoad,
+            'event_category': 'JS Dependencies'
+        });
+    }
 });
