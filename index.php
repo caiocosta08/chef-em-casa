cbf3597856513211154436c0fca745538b674dc3
Caio <caiocostaee@gmail.com>
2018-02-19 15:29:17 -0300

    definindo favicon

--- index.php
+++ index.php
@@ -19,6 +19,7 @@
 		<meta name="description" content="" />
 		<link href="css/default.css" rel="stylesheet" type="text/css" />
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+        <link rel="shortcut icon" href="favicon.ico" />
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
