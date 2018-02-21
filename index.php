28c6e0e913c3143df64d4aea385e75f7a1090d04
Caio <caiocostaee@gmail.com>
2018-02-21 17:41:27 -0300

    alterando arquivos para colocar uma página inicial (De abertura)

--- index.php
+++ index.php
@@ -80,7 +80,7 @@
 						}else include('forms/login.php');
 					?>
                     <div class="carousel-item">
-                      <img src="crucifixo-1.jpg" alt="...">
+                      <img src="img/crucifixo-1.jpg" alt="...">
                         <div class="carousel-caption d-none d-md-block">
                         <h5>...</h5>
                         <p>...</p>
