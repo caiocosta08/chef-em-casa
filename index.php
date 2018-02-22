a1131d9c188a9b103a80e45595f7436a02eeb1e6
Caio <caiocostaee@gmail.com>
2018-02-22 10:47:41 -0300

    adicionando imagens

--- index.php
+++ index.php
@@ -76,7 +76,7 @@
 							echo '<button id="btnLogout" type="button" name="button"><a href="php/logout.php">LOGOUT</a> </button>';
 						}else include('forms/login.php');
 					?>
-                    <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+                    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
                       <div class="carousel-inner">
                         <div class="carousel-item active">
                           <img class="d-block w-100" src="img/burger.jpg" alt="First slide">
@@ -88,6 +88,14 @@
                           <img class="d-block w-100" src="img/burger.jpg" alt="Third slide">
                         </div>
                       </div>
+                      <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                      </a>
+                      <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                      </a>
                     </div>
 				</div>
 				<div class="col-md-2 sidenav">
