401483dd6712491d2e4aadc343306170d9c2e8a1
Caio <caiocostaee@gmail.com>
2018-02-22 11:10:02 -0300

    adicionando imagens

--- index.php
+++ index.php
@@ -109,40 +109,6 @@
                         <span class="sr-only">Next</span>
                       </a>
                     </div>
-                    <!-- ----------------------------------------------------------------------------------------- -->
-                    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-                      <div class="carousel-inner">
-                        <div class="carousel-item">
-                          <img src="img/burger.jpg" alt="...">
-                          <div class="carousel-caption d-none d-md-block">
-                            <h5>Título do item 1</h5>
-                            <p>Descrição lorem lorem lorem ipsum lorem </p>
-                          </div>
-                        </div>
-                        <div class="carousel-item">
-                          <img src="img/burger.jpg" alt="...">
-                          <div class="carousel-caption d-none d-md-block">
-                            <h5>Título do item 1</h5>
-                            <p>Descrição lorem lorem lorem ipsum lorem </p>
-                          </div>
-                        </div>
-                        <div class="carousel-item">
-                          <img src="img/burger.jpg" alt="...">
-                          <div class="carousel-caption d-none d-md-block">
-                            <h5>Título do item 1</h5>
-                            <p>Descrição lorem lorem lorem ipsum lorem </p>
-                          </div>
-                        </div>
-                      </div>
-                      <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                      </a>
-                      <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                      </a>
-                    </div>
 				</div>
 				<div class="col-md-2 sidenav">
 					<ul id="contentNews" style="list-style: none; padding: 0;"></ul>
