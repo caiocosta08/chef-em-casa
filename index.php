6c36817c29fc24f3c154a833de1a834c38f85ca9
Caio <caiocostaee@gmail.com>
2018-02-22 11:17:58 -0300

    adicionando imagens ao carousel

--- index.php
+++ index.php
@@ -86,16 +86,23 @@
                           </div>
                         </div>
                         <div class="carousel-item">
-                          <img class="d-block w-100" src="img/burger.jpg" alt="Second slide">
+                          <img class="d-block w-100" src="img/cake.jpg" alt="Second slide">
                           <div class="carousel-caption d-none d-md-block">
-                            <h5>Título do item 1</h5>
+                            <h5>Título do item 2</h5>
                             <p>Descrição lorem lorem lorem ipsum lorem </p>
                           </div>
                         </div>
                         <div class="carousel-item">
-                          <img class="d-block w-100" src="img/burger.jpg" alt="Second slide">
+                          <img class="d-block w-100" src="img/pizza.jpg" alt="Second slide">
                           <div class="carousel-caption d-none d-md-block">
-                            <h5>Título do item 1</h5>
+                            <h5>Título do item 3</h5>
+                            <p>Descrição lorem lorem lorem ipsum lorem </p>
+                          </div>
+                        </div>
+                        <div class="carousel-item">
+                          <img class="d-block w-100" src="img/food.jpg" alt="Second slide">
+                          <div class="carousel-caption d-none d-md-block">
+                            <h5>Título do item 4</h5>
                             <p>Descrição lorem lorem lorem ipsum lorem </p>
                           </div>
                         </div>
