06971c9b026cc4ec2c62f0bdf9478958db90a99a
Caio <caiocostaee@gmail.com>
2018-02-22 10:54:21 -0300

    adicionando imagens

--- index.php
+++ index.php
@@ -78,14 +78,26 @@
 					?>
                     <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
                       <div class="carousel-inner">
-                        <div class="carousel-item active">
-                          <img class="d-block w-100" src="img/burger.jpg" alt="First slide">
+                        <div class="carousel-item">
+                          <img src="img/burger.jpg" alt="...">
+                          <div class="carousel-caption d-none d-md-block">
+                            <h5>Título do item 1</h5>
+                            <p>Descrição lorem lorem lorem ipsum lorem </p>
+                          </div>
                         </div>
                         <div class="carousel-item">
-                          <img class="d-block w-100" src="img/burger.jpg" alt="Second slide">
+                          <img src="img/burger.jpg" alt="...">
+                          <div class="carousel-caption d-none d-md-block">
+                            <h5>Título do item 1</h5>
+                            <p>Descrição lorem lorem lorem ipsum lorem </p>
+                          </div>
                         </div>
                         <div class="carousel-item">
-                          <img class="d-block w-100" src="img/burger.jpg" alt="Third slide">
+                          <img src="img/burger.jpg" alt="...">
+                          <div class="carousel-caption d-none d-md-block">
+                            <h5>Título do item 1</h5>
+                            <p>Descrição lorem lorem lorem ipsum lorem </p>
+                          </div>
                         </div>
                       </div>
                       <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
