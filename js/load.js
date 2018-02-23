82ddecca1a41efeef99bad5fe5a98f507dad5ade
Caio <caiocostaee@gmail.com>
2018-02-22 22:13:52 -0300

    add infinite scroll

--- js/load.js
+++ js/load.js
@@ -44,10 +44,10 @@ function loadChefs(){
     if(document.title == 'Chef em Casa - CHEFS'){
       $('#chefsRegistrados').html(chefs);
         $("#pag").click(function(){
-            for(let i=0; i<3; i = i + 1){
-                 if(vetor[i] != '' || vetor[i] != null){ 
+            for(let i=0; i<3; i++){
+                 //if(vetor[i] != '' || vetor[i] != null){ 
                      $('#chefsRegistrados').append('<li class="list-group-item">' + vetor[i].id + vetor[i].titulo + '</li>');
-                }
+                //}
                 vetor.shift();
             }    
         })
