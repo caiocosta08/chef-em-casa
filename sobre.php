b8143f7af47241e86da3fd8e3cc6360fa7ddc7f7
Caio <caiocostaee@gmail.com>
2018-02-23 09:51:39 -0300

    atualizando arquivos para finalizar

--- sobre.php
+++ sobre.php
@@ -1,139 +1,107 @@
 <?php include_once('php/sessao.php') ?>
 <!DOCTYPE html>
-<head>
-<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Chef em Casa - ADMIN</title>
-<meta name="keywords" content="" />
-<meta name="description" content="" />
-<link href="css/default.css" rel="stylesheet" type="text/css" />
-<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-<script src="js/jquery-3.2.1.js" type="text/javascript"></script>
-<script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-<script src="js/load.js" type="text/javascript"></script>
-<script src="js/functions.js" type="text/javascript"></script>
-<script src="js/validator.js" type="text/javascript"></script>
-<script src='https://www.google.com/recaptcha/api.js'></script>
-<style rel="stylesheet" type="text/css">
-	iframe{
-		width: 100px;
-		margin: auto;
-	}
-	.navbar {
-		background-color: #fff353;
-		border-color: #8a6d3b;
-	}
-	.navbar-inverse .navbar-nav>li>a{
-		color: #8a6d3b;
-	}
-	.navbar-inverse .navbar-nav>li>a:hover{
-		color: #333333;
-	}
-	.navbar-inverse .navbar-nav>.active>a, .navbar-inverse .navbar-nav>.active>a:focus, .navbar-inverse .navbar-nav>.active>a:hover{
-		background-color: #8a6d3b;
-	}
-	.navbar-inverse .navbar-brand{
-		color: #8a6d3b;
-	}
-	.navbar-inverse .navbar-brand:hover{
-		color: #333333;
-	}
-	body {
-		color: #8a6d3b;
-	}
-	blockquote small {
-		color: #8a6d3b;
-	}
-	.content {
-		/*background-color: #fff1a8;
-	*/}
-	.well {
-		background-color: #b99c6b;
-		border-color: #8a6d3b;
-		color: #d5c4a1;
-	}
-	.well a{
-		color: white;
-	}
-	.page-header {
-		color: gray;
-		background-color: yellow;
-		border-color: yellow;
-	}
-</style>
-</head>
-<body style="margin: 1%;">
-	<nav class="navbar navbar-inverse">
-	<div class="container-fluid">
-		<div class="navbar-header">
-			<a class="navbar-brand" href="#">Chef em Casa</a>
-		</div>
-		<ul class="nav navbar-nav">
-			<li class="active"><a href="index.php"><span class="glyphicon glyphicon-home"></span> Home</a></li>
-			<li><a href="chefs.php"><span class="glyphicon glyphicon-pencil"></span> Chefs</a></li>
-			<li><a href="calendario.php"><span class="glyphicon glyphicon-calendar"></span> Calendário</a></li>
-			<li><a href="admin.php"><span class="glyphicon glyphicon-cog"></span> Admin</a></li>
-			<li><a href="sobre.php"><span class="glyphicon glyphicon-info-sign"></span> Sobre</a></li>
-		</ul>
-		<ul class="nav navbar-nav navbar-right">
-			<?php if($logado == '' || $logado == null) echo '<li><a href="#" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-user"></span> Cadastrar</a></li>'?>
-		</ul>
-	</div>
-	</nav>
-	<?php include('modal/modal-add-user.php'); ?>
-<div class="container-fluid text-center">
-  <div class="row content">
-		<div class="col-sm-2 sidenav well">
-			<?php
-                //$logado = $_SESSION['login'];
-				if($logado != '' && $logado != null){
-					echo '<span style="font-size: 50px;" class="glyphicon glyphicon-user"></span>';
-					echo '<h3>Olá, <b>' . $logado . '</b>!</h3>';
-					echo '<button type="button" name="button"><a href="php/logout.php">LOGOUT</a> </button>';
-				}else include('forms/login.php');
-			?>
-		</div>
-    <div class="col-sm-8 text-left">
-      <h1>Bem-vindo!</h1>
-      <p>
-				Olá! Nós somos o <code>CHEF EM CASA</code> e damos a você as boas vindas ao
-				nosso website oficial! Após muitas orações e dedicação de muitos, nós
-				alcançamos a graça de publicar o nosso portal para os nossos membros
-				terem acesso a todo conteúdo formativo disponibilizado por nós. Para ter
-				acesso você precisa possuir um <kbd>login</kbd> e uma <kbd>senha</kbd>.
-			</p>
+	<head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-114078381-1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
 
-      <hr>
-			<h2>Instruções</h2>
-				<h4>Login</h4>
-					<blockquote>
-						<p><small>
-							Digite o <kbd>login</kbd> e a <kbd>senha</kbd> na barra de navegação que
-							está no topo da página. Após isso, aperte na tecla <code>ENTER</code> no
-							seu teclado ou no botão <span class="glyphicon glyphicon-log-in"></span>
-							que está ao lado das caixas de digitação.
-						</small></p>
-					</blockquote>
-				<h4>Cadastro</h4>
-					<blockquote>
-						<p><small>
-							Clique em <kbd>Cadastrar</kbd> na barra de navegação que está no topo
-							da página. Após isso, você será redirecionado para a página de cadastro
-							e deverá preencher corretament os campos com suas informações para
-							finalizar o	cadastro e possuir acesso ao site.
-						</small></p>
-					</blockquote>
-			<hr>
-    </div>
-		<div class="col-sm-2 sidenav">
-			<ul id="contentNews" style="list-style: none; padding: 0;">
-			</ul>
-		</div>
-	</div>
-</div>
+          gtag('config', 'UA-114078381-1');
+        </script>
 
-<footer class="container-fluid text-center">
-	<p>Copyright &copy; 2017 Chef em Casa. All Rights Reserved.</p>
-	<p><a href="#">Privacy Policy</a> | <a href="#">Terms of Use</a></p>
-</footer>
+
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Chef em Casa - SOBRE</title>
+		<meta name="keywords" content="" />
+		<meta name="description" content="" />
+		<link href="css/default.css" rel="stylesheet" type="text/css" />
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+        <link rel="icon" href="favicon.ico" />
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+        
+    </head>
+	
+	<body>
+        
+		<?php include('modal/modal-add-user.php'); ?>
+        
+        <div style="background-image: url('img/burger.jpg'); width: 100%; height: 700px; background-size: 300%; background-position: top; background-repeat: no-repeat; display: flex; flex-direction: column; align-items: center; align-content: center; padding-top: 30%; color: #ffffff;" >
+            <div style="background-color: rgba(0,0,0,0.5); color: #FFFFFF; width: 100%; height: 50%; padding: 50px;">
+                <h1> CHEF EM CASA</h1>
+                <h3> O maior portal de Chefs de cozinha do Brasil!</h3>
+            </div>
+        </div>
+		
+        <nav class="navbar navbar-expand-sm navbar-dark" style="background: coral; margin-bottom: 20px;">
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-content" aria-controls="nav-content" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+
+			<!-- Brand/Título -->
+			<a class="navbar-brand" href="index.php">Chef em Casa</a>
+
+			<!-- Links -->
+			<div class="collapse navbar-collapse" id="nav-content">   
+				<ul class="navbar-nav">
+					<li class="nav-item active">
+						<a class="nav-link" href="index.php">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="chefs.php">Chefs</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="calendario.php">Calendário</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="admin.php">Admin</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="sobre.php">Sobre</a>
+					</li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<?php if($logado == '' && $logado == null) echo '<li><a href="#"><span class="glyphicon glyphicon-user"></span> Cadastrar</a></li>'?>
+				</ul>
+			</div>
+		</nav>
+        
+        <div class="container-fluid text-center">
+			<div class="row">
+				<div class="col-md-2 sidenav">
+					<ul id="contentNews" style="list-style: none; padding: 0;"></ul>
+				</div>
+				<div class="col-md-8 text-left">
+					<h2>Olá! Seja bem vindo ao nosso portal Chef em Casa.</h2>
+					<hr>
+					<h2>Quem somos?</h2>
+					<h3>
+						Somos o maior portal de Chefs de Cozinha do Brasil! Aqui você encontra os profissionais mais 
+						capacitados para trabalharem no seu evento de acordo com sua necessidade. Chefs com as mais variadas habilidades 
+						disponíveis para lhe darem a maior assistência possível quando se fala de comida! 
+					</h3>
+				</div>
+				<div class="col-md-2 sidenav">
+					<div class="card" style="width: 100%;"> 
+                        <div class="card-body">
+                            <h5 class="card-title"> titulo do card </h5>
+                            <h6 class="card-subtitle mb-2 text-muted"> descrição </h6>
+                            <p class="card-text"><a href="#"> link </a></p>
+                        </div>
+                    </div>
+				</div>
+			</div>
+        </div>
+		
+        <script src="js/load.js" type="text/javascript"></script>
+		<script src="js/functions.js" type="text/javascript"></script>
+		<script src="js/validator.js" type="text/javascript"></script>
+        <script src="js/img.js" type="text/javascript"></script>
+		<script src="js/ana.js" type="text/javascript"></script>
+        <script src='https://www.google.com/recaptcha/api.js'></script>
+	</body>
 </html>
