830bf07fef54315829d37d03ab5f23064cafb131
Caio <caiocostaee@gmail.com>
2018-02-23 14:21:40 -0300

    editando recaptcha

--- index.php
+++ index.php
@@ -90,7 +90,7 @@
                 	    <div class="card-body">
                             <h5 class="card-title"> Hambúrguer! </h5>
                             <h6 class="card-subtitle mb-2 text-muted"> Hambúrguer artesanal </h6>
-                            <p class="card-text"><a href="#"> <img src="img/burger.jpg"> link </a></p>
+                            <p class="card-text"><a href="#"> <img src="img/burger.jpg"></a></p>
                         </div>
                     </div>
 					<div class="card" style="width: 100%;"> 
