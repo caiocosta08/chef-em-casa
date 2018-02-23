4e2f0ad002eaab2ca00673ee91c1e6227ef9cca4
Caio <caiocostaee@gmail.com>
2018-02-23 13:03:32 -0300

    webpack sem jquery

--- index.php
+++ index.php
@@ -20,6 +20,8 @@
 		<link href="css/default.css" rel="stylesheet" type="text/css" />
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
         <link rel="icon" href="favicon.ico" />
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+
     </head>
 	
 	<body>
